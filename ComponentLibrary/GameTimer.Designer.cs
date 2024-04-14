9668a270f60c9fb70b03970b9e460e3153b59de5
Xenia <166983966+Xenia135@users.noreply.github.com>
2024-04-14 15:11:43 +0300

    Add files via upload

---
+++ ComponentLibrary/GameTimer.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ComponentLibrary
+{
+    partial class GameTimer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.display = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.time_Tick);
+            // 
+            // display
+            // 
+            this.display.AutoSize = true;
+            this.display.Location = new System.Drawing.Point(3, 0);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(55, 16);
+            this.display.TabIndex = 0;
+            this.display.Text = "00:00:00";
+            this.display.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // GameTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.display);
+            this.Name = "GameTimer";
+            this.Size = new System.Drawing.Size(60, 19);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label display;
+    }
+}
---
+++ ComponentLibrary/GameTimer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComponentLibrary
+{
+    public partial class GameTimer: UserControl
+    {
+        public GameTimer()
+        {
+            InitializeComponent();
+        }
+
+        public void Start()
+        {
+            display.Text = "00:00:00";
+        }
+
+        public DateTime date;
+        public void StartGame()
+        {
+            display.Text = "00:00:00";
+            timer1.Enabled = true;
+            date = new DateTime();
+        }
+        public void Stop()
+        {
+            timer1.Enabled= false;
+            display.Text="00:00:00";
+        }
+        private void time_Tick(object sender,EventArgs e)
+        {
+            date = date.AddSeconds(1);
+            display.Text=date.ToString("HH:mm:ss");
+        }
+
+        public string GetTimer()
+        { return display.Text; }
+    }
+}
